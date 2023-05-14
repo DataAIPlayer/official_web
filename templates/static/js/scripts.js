1d414ba093c7b170b27886458ae85fcce2554cdd
zhaobin <zhaobinchu.go@gmail.com>
2023-05-14 19:51:19 +0800

    v1

---
+++ templates/static/js/scripts.js
@@ -0,0 +1,36 @@
+const textElement = document.getElementById("text");
+const textArray = [
+    "欢迎来到我们的人工智能公司...",
+    "我们专注于AI技术...",
+    "为您提供高质量的人工智能服务...",
+];
+const typingDelay = 200;
+const erasingDelay = 100;
+const newTextDelay = 2000;
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+    if (charIndex < textArray[textArrayIndex].length) {
+        textElement.textContent += textArray[textArrayIndex].charAt(charIndex);
+        charIndex++;
+        setTimeout(type, typingDelay);
+    } else {
+        setTimeout(erase, newTextDelay);
+    }
+}
+
+function erase() {
+    if (charIndex > 0) {
+        textElement.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
+        charIndex--;
+        setTimeout(erase, erasingDelay);
+    } else {
+        textArrayIndex = (textArrayIndex + 1) % textArray.length;
+        setTimeout(type, typingDelay + 1100);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(type, newTextDelay + 250);
+});
